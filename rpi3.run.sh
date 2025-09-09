61a440496df3121edb3305fd7d3a86a67b997826
alfonzso <alfonzso@gmail.com>
2025-09-09 21:56:14 +0200

    refactorign

--- rpi3.run.sh
+++
@@ -1,3 +0,0 @@
-sudo nixos-rebuild --flake .#rpi --use-remote-sudo --target-host nxadmin@nxrpi3 switch
-
-# sudo nixos-rebuild --flake .#rpi --use-remote-sudo --target-host nxadmin@rpi3Nix switch
--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 # nixos-rebuild --flake .#rpi --target-host root@nxrpi3 build --impure
 # nixos-rebuild --flake .#rpi --target-host root@nxrpi3 switch --impure
 
+nixos-rebuild --flake .#rpi --use-remote-sudo --target-host nxadmin@nxrpi3 switch
